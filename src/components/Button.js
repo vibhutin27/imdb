356ca2294a08328ae96408f2ad331b0f370bf1ad
vibhutin27 <vibhutin27@gmail.com>
2024-12-05 19:13:15 +0530

    pageNotFoundupdated

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
-import React from 'react'
 
-export const Button = () => {
+
+export const Button = ({children}) => {
   return (
-    <div>Button</div>
+    <button className='px-4 py-2 bg-gray-200 text-gray-800 font-medium rounded-lg hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2'>{children}</button>
   )
 }
 
